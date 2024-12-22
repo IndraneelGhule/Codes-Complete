ba9a0ad5d11bb6a682ab9ea9e1d918c04c8b08ac
IndraneelGhule <ghule.indraneel@gmail.com>
2024-12-23 02:40:09 +0530

    I599

--- src/F1/Testing5.java
+++ src/F1/Testing5.java
@@ -7,7 +7,7 @@ public class Testing5 {
 	@Test
 	public void launchChrome() {
 		
-		System.out.println("****************IG*11********Edited*******");
+		System.out.println("****************IG*6666********Edited*******");
 	}
 	
 }
